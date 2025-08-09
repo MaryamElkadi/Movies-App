6bd5c0f400b61c4f19fedc5c28b8d5cfc89316ff
maryam <maryam.khamis.elkadi@gmail.com>
2025-08-09 15:19:17 +0300

    Initial commit with Tailwind CSS setup

---
+++ tailwand.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff2c1f',
+        'text-color': '#020307',
+        'bg-color': '#fff',
+      },
+      spacing: {
+        '4.5': '1.125rem',
+      },
+      borderRadius: {
+        '5xl': '5rem',
+      },
+      scrollPadding: {
+        'top': '2rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
